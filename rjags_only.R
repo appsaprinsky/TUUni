86d92cdb9e8cf932938cfbebda7c5f3861797092
appsaprinsky <app1.n.saprinsky@gmail.com>
2023-10-21 21:30:25 +0200

    Step 1
    Initialize rjags

--- rjags_only.R
+++ rjags_only.R
@@ -1,15 +1,8 @@
-# install.packages("rstan")
 install.packages("R2jags")
-install.packages("rjags")
-# install.packages("blavaan")
-# install.packages("mvtnorm")
+install.packages("mvtnorm")
 
-library(rjags) # issue with jags instalation
 library(R2jags)
-# library(blavaan) # standard package
-# library(rstan)
 library(mvtnorm) # mv normal data
-packageVersion("rjags")
 
 # BNFI, BTLI, BCFI, BMc, BΓadj, BΓˆ, BRMSEA //// PPPχ2
 ###########################################
@@ -49,33 +42,139 @@ model_real_jags <- jags.parallel(data=data.jags,
                         n.iter=2000, n.chains=4,n.thin=1,n.burnin = 1000,
                         model.file="model1_cfa.txt")
 model_real_jags
-est1 <- model_real_jags$BUGSoutput$summary
+'
+For each parameter, n.eff is a crude measure of effective sample size,
+and Rhat is the potential scale reduction factor (at convergence, Rhat=1).
+
+DIC info (using the rule, pD = var(deviance)/2)
+pD = 1358.2 and DIC = 5616.4
+DIC is an estimate of expected predictive error (lower deviance is better).
+'
 
 ###########################################
 # data analysis with fake data
 ###########################################
 
-
 # 2nd model: jags
-# 3rd model: stan
+data.jags <- list(N=N,           #sample size
+                  y=x_fake,         #data
+                  mu.0=c(0,0),   #zero-vector
+                  phi=phi) #diagonal matrix
 
-
-
+params <- c("ly","sigma.eps","sigma.eta")
+model_real_jags <- jags.parallel(data=data.jags, 
+                                 parameters.to.save=params,
+                                 n.iter=2000, n.chains=4,n.thin=1,n.burnin = 1000,
+                                 model.file="model1_cfa.txt")
+model_real_jags
 '
-Questions to ask:
-1) How to make BRMSEA  non NaN?
-3) Am I doing it correctly so far?
-4) issue with jags instalation
-5) cloud tempo Solution?
-6) Group Partner
-7) # fitmeasures(res2) is not working anymore? 
-8) Stan Solution problematic? not sure if correct, several resources were used.
-9) Feeling the Form
+For each parameter, n.eff is a crude measure of effective sample size,
+and Rhat is the potential scale reduction factor (at convergence, Rhat=1).
 
-What to do for Step 1 Project:
-4) Stan write the code
+DIC info (using the rule, pD = var(deviance)/2)
+pD = 1358.2 and DIC = 5616.4
+DIC is an estimate of expected predictive error (lower deviance is better).
+'
 
-''
+
+
+###########################################
+# STEP 2
+###########################################
+library(R2jags)
+install.packages("loo")
+library(loo)
+
+###############################################################
+# DATA
+# simulated data set based on Flückiger et al. (2022)
+###############################################################
+data.jags <- list(N=N,           #sample size
+                  y=x,         #data
+                  mu.0=c(0,0),   #zero-vector
+                  phi=phi) #diagonal matrix
+
+params <- c("ly","sigma.eps","sigma.eta")
+
+###############################################################
+# RUN MODELS
+###############################################################
+model1 <- jags.parallel(data=data.jags, 
+                        parameters.to.save=params,
+                        n.iter=4000, n.chains=3,n.thin=1,n.burnin = 2000,
+                        model.file="model1_dsem.txt")
+
+data.jags <- list(N=N,           #sample size
+                  y=x_fake,         #data
+                  mu.0=c(0,0),   #zero-vector
+                  phi=phi) #diagonal matrix
+
+
+model2 <- jags.parallel(data=data.jags, 
+                        parameters.to.save=params,
+                        n.iter=4000, n.chains=3,n.thin=1,n.burnin = 2000,
+                        model.file="model1_dsem.txt")
+
+###############################################################
+samps <- as.mcmc(model1)
+#xyplot, densityplot
+pdf(file="xyplot.pdf",pointsize=6)
+plot(samps)
+dev.off()
+
+pdf("gelmanplot.pdf")
+par(new=F)
+for (v in 1:nvar(samps)){
+  gelman.plot(samps[,v],ylim=c(0,7),col=c("black",NA))
+  par(new=T)
+}
+abline(h=1.1)
+dev.off()
+###############################################################
+
+est1 <- model1$BUGSoutput$summary
+est2 <- model2$BUGSoutput$summary
+round(est1,2)
+round(est2,2)
+
+
+# extract likelihoods
+niter <- 2000
+nchain <- 3
+mc_ll1 <- mc_ll2 <- array(NA,c(niter,nchain,N))
+
+samps1 <-  as.mcmc(model1)
+samps2 <-  as.mcmc(model2)
+
+
+for(r in 1:nchain){
+  mc_ll1[,r,] <-  samps1[[r]][,paste0("loglik1[",1:N,"]")]
+  mc_ll2[,r,] <-  samps2[[r]][,paste0("loglik1[",1:N,"]")]
+}
+
+# model fit indices are similar to information criteria (AIC, BIC)
+waic1 <- waic(mc_ll1)
+waic2 <- waic(mc_ll2)
+loo1 <- loo(mc_ll1)
+loo2 <- loo(mc_ll2)
+
+# WAIC
+print(loo_compare(waic1, waic2), digits = 2)
+# model 3 is fitting best
+
+# loo
+print(loo_compare(loo1, loo2), digits = 2)
+# same here
+
+# DIC
+model1$BUGSoutput$DIC
+model2$BUGSoutput$DIC
+
+
+
+
+
+
 
 
 
